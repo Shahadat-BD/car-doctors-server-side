f79aed770bc0bce678b479e6a9496ed63ced4b5d
Shahadat-BD <shahadatstudent34@gmail.com>
2023-11-03 11:11:29 -0700

    jwt api added

--- index.js
+++ index.js
@@ -3,11 +3,19 @@ const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
+const jwt = require('jsonwebtoken');
+const cookieParser = require("cookie-parser")
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+     origin : [ 
+      'http://localhost:5173'
+     ],
+     credentials : true
+}))
 app.use(express.json())
+app.use(cookieParser())
 
 
 
@@ -22,6 +30,29 @@ const client = new MongoClient(uri, {
   }
 });
 
+
+// Middlewares 
+
+const logger = (req,res,next) => {
+     console.log('log info :',req.method , req.url);
+     next()
+}
+
+const verifyToken = (req,res,next) =>{
+    const token = req?.cookies?.token
+    // console.log('token in the middleware',token);
+   if (!token) {
+        return res.status(401).send({message : "unauthorized access"})
+   }
+   jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded) => {
+       if (err) {
+          return res.status(401).send({message : "unauthorized access"})
+       }
+       req.user = decoded;
+       next()
+   })
+}
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -30,6 +61,31 @@ async function run() {
     const serviceCollection = client.db("CarDoctors").collection('services');
     const orderCollection   = client.db("CarDoctors").collection('serviceOrder');
   
+
+//  auth related api 
+
+app.post("/jwt",async(req,res) => {
+   const user = req.body 
+   console.log('user token response',user);
+   const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET , {expiresIn:'1h'})
+   
+   res.cookie('token',token,{
+        httpOnly : true,
+        secure : true,
+        sameSite : 'none'
+   })
+   .send({success : true})
+})
+
+
+// logout api => when user logout then token get out form cooke.
+   app.post('/logout',async(req,res) => {
+       const user  =  req.body;
+       console.log('logout user',user);
+       res.clearCookie('token',{maxAge:0})
+       .send({success : true})
+   })
+
     // the user service order stored in database by post method.
     app.post('/order',async(req,res)=>{
       const order = req.body
@@ -38,7 +94,11 @@ async function run() {
     })
 
     // the user services order collect form database by get method for display in client side.
-    app.get('/order',async(req,res)=>{
+    app.get('/order',logger, verifyToken ,async(req,res)=>{
+      console.log('user token owner',req.user);
+       if (req.user.email !== req.query.email) {
+          res.status(403).send({message : "forbidden access"})
+       }
        let query = {}
        if (req.query?.email) {
           query = { email : req.query.email }
@@ -46,18 +106,10 @@ async function run() {
        const result = await orderCollection.find(query).toArray()
        res.send(result)
     })
-  // specific order service collect by get method 
-    app.get('/order/:id',async(req,res) => { 
-      const id = req.params.id
-      const query = {_id : new ObjectId(id) }
-      const result = await orderCollection.findOne(query)
-      res.send(result)
-  })
-
     // specific order deleted by delete method
     app.delete('/order/:id',async(req,res)=>{
       const id = req.params.id
-      const query = { _id : new ObjectId(id) };
+      const query = { _id : new ObjectId(id) };  
       const result = await orderCollection.deleteOne(query);
         res.send(result)
     })
